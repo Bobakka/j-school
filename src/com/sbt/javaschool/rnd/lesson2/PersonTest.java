1b6ffb3ab6d813e426509055e69636ad11ec9c0f
Bobakka <antigalaxy@gmail.com>
2019-11-01 04:26:36 +0300

    Lesson2: add Person class. Test this class

---
+++ src/com/sbt/javaschool/rnd/lesson2/PersonTest.java
@@ -0,0 +1,49 @@
+package com.sbt.javaschool.rnd.lesson2;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void testEqualMan() {
+        Person man1 = new Person(true, "Ivan Ivanov");
+        Person man2 = new Person(true, "Petya Petrov");
+
+       assertFalse(man1.marry(man2));
+    }
+
+    @Test
+    void testMarry() {
+        Person Mary = new Person(false, "Marry");
+        Person Sam = new Person(true, "Sam");
+
+        assertTrue(Mary.marry(Sam));
+        assertEquals(Mary, Sam.getSpouse());
+        assertEquals(Sam, Mary.getSpouse());
+    }
+    @Test
+    void testDoubleMarry() {
+        Person Mary = new Person(false, "Marry");
+        Person Sam = new Person(true, "Sam");
+
+        assertTrue(Mary.marry(Sam));
+        assertFalse(Mary.marry(Sam));
+    }
+
+    @Test
+    void testMaryAndDivorce() {
+        Person Mary = new Person(false, "Marry");
+        Person Sam = new Person(true, "Sam");
+        Person Philip = new Person(true, "Philip");
+
+        Mary.marry(Sam);
+
+        assertTrue(Mary.marry(Philip));
+
+        assertNull(Sam.getSpouse());
+        assertEquals(Mary.getSpouse(), Philip);
+        assertEquals(Philip.getSpouse(), Mary);
+    }
+}
