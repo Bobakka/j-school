73bbc3ecb7415746844367dd17e10005b20c59fc
Bobakka <antigalaxy@gmail.com>
2019-11-04 17:40:07 +0300

    Lesson3: add class which works with Collections

---
+++ src/com/sbt/javaschool/rnd/lesson3/CollectionWorker.java
@@ -0,0 +1,103 @@
+package com.sbt.javaschool.rnd.lesson3;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
+public class CollectionWorker {
+
+    private TreeSet<String> uniqueWords;
+    private ArrayList<String> words;
+    private TreeMap<String, Integer> countWords;
+    private ArrayList<String> lines;
+
+    public CollectionWorker(InputStream stream) throws IOException {
+        uniqueWords =  new TreeSet<>();
+        words = new ArrayList<>();
+        countWords = new TreeMap<>();
+        lines = new ArrayList<>();
+        readFile(stream);
+    }
+    public boolean readFile(InputStream stream) throws IOException {
+        Scanner scanner;
+        try {
+            scanner = new Scanner(stream);
+/*read from files as lines,
+* after then read words from lines
+* and add our containers
+ */
+            while(scanner.hasNext()) {
+                String lineValue = scanner.nextLine();
+                lines.add(lineValue);
+                Scanner scannerLine = new Scanner(lineValue);
+                while(scannerLine.hasNext()) {
+                    String value = scannerLine.next();
+                    uniqueWords.add(value.toLowerCase());
+                    words.add(value.toLowerCase());
+                    if ((countWords.containsKey(value))) {
+                        countWords.put(value, countWords.get(value) + 1);
+                    } else {
+                        countWords.put(value, 1);
+                    }
+                }
+            }
+            return true;
+        } finally {
+            if (stream != null)
+                stream.close();
+        }
+
+    }
+
+//Task1
+    public Integer numberDifferentWords() {
+        return uniqueWords.size();
+    }
+//Task2.1
+    public String getUniqueWordsOrderBySize() {
+
+        StringJoiner result = new StringJoiner(" ");
+        ArrayList<String> tmp = new ArrayList<>(uniqueWords);
+        tmp.sort(Comparator.comparingInt(String::length));
+        for (String s : tmp) {
+            result.add(s);
+        }
+
+        return result.toString();
+    }
+ //Task2.2
+ public String getUniqueWordsOrderByAlphabetical(){
+     StringJoiner result = new StringJoiner(" ");
+     for(String s : uniqueWords)
+         result.add(s);
+
+     return result.toString();
+ }
+//Task3
+    public String getStatisticWords() {
+        StringJoiner result = new StringJoiner("\n");
+        countWords.forEach((k,v)->result.add(k + ": " + v));
+
+        return result.toString();
+    }
+//Task4
+    public String reverseText() {
+        StringJoiner result = new StringJoiner("\n");
+        ArrayList<String> tmp = lines;
+        Collections.reverse(tmp);
+
+        for (String line : tmp) {
+            result.add(line);
+        }
+
+        return result.toString();
+    }
+//Task6
+    public String getLine(Integer iLine) throws ArrayIndexOutOfBoundsException{
+
+        if (iLine > lines.size() - 1)
+            throw new ArrayIndexOutOfBoundsException();
+        return lines.get(iLine);
+    }
+
+}
---
+++ src/com/sbt/javaschool/rnd/lesson3/CollectionWorkerTest.java
@@ -0,0 +1,103 @@
+package com.sbt.javaschool.rnd.lesson3;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class CollectionWorkerTest {
+    private static CollectionWorker w;
+    private static  String inputString;
+
+    @BeforeAll
+    static void init() throws IOException {
+        String fName = "c:/tmp/example.txt";
+        FileInputStream in = new FileInputStream(fName);
+
+        inputString = "a aaaa aa aaa qwerrtt\ndfdffda sda aaaa dfdf";
+        w = new CollectionWorker(
+                new ByteArrayInputStream(inputString.getBytes(Charset.forName("UTF-8")))
+        );
+    }
+
+    @Test
+    void readFile() {
+    }
+
+    @DisplayName("Test Task 1")
+    @Test
+    void numberDifferentWords() {
+        final Integer standardResult = 8;
+        assertEquals(w.numberDifferentWords(), standardResult);
+    }
+
+    @DisplayName("Test Task 2.1")
+    @Test
+    void getUniqueWordsOrderBySize() {
+        final String expect = "a aa aaa sda aaaa dfdf dfdffda qwerrtt";
+        String result = w.getUniqueWordsOrderBySize();
+        assertEquals(result, expect);
+    }
+
+    @DisplayName("Test Task 2.2")
+    @Test
+    void getUniqueWordsOrderByAlphabetical() {
+        final String expect = "a aa aaa aaaa dfdf dfdffda qwerrtt sda";
+        String result = w.getUniqueWordsOrderByAlphabetical();
+        assertEquals(result, expect);
+    }
+
+    @DisplayName("Test Task 3")
+    @Test
+    void getStatisticWords() {
+        final String expect = "a: 1\naa: 1\naaa: 1\naaaa: 2\ndfdf: 1\ndfdffda: 1\nqwerrtt: 1\nsda: 1";
+        String result = w.getStatisticWords();
+        assertEquals(result, expect);
+    }
+
+    @DisplayName("Test Task 4")
+    @Test
+    void reverseText() {
+        String expect = "dfdffda sda aaaa dfdf\na aaaa aa aaa qwerrtt";
+
+        String result = w.reverseText();
+        assertEquals(result, expect);
+    }
+
+    @DisplayName("Test Task 6")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2})
+    void getLine(int idx) {
+        String expect;
+        switch (idx) {
+            case 0:
+                expect = "a aaaa aa aaa qwerrtt";
+                break;
+            case 1:
+                expect = "dfdffda sda aaaa dfdf";
+                break;
+            default:
+                expect="";
+        }
+        if (idx > 1) {
+            assertThrows(ArrayIndexOutOfBoundsException.class,()->w.getLine(idx));
+        }
+        else {
+            String result = w.getLine(idx);
+            assertEquals(result, expect);
+        }
+
+
+
+
+    }
+}
